b4bfacd5259211ca31d756600af99bf673caefe0
Emba Moussa <emba.moussa@eventinc.de>
2018-06-07 18:57:47 +0200

    feat: solve apple orange challenge

---
+++ algorithms/implementation/apple-and-orange.rb
@@ -0,0 +1,35 @@
+#!/bin/ruby
+
+require 'pry'
+require 'json'
+require 'stringio'
+
+# Complete the countApplesAndOranges function below.
+def countApplesAndOranges(s, t, a, b, apples, oranges)
+  p apples.select {|app| s <= a+app && a+app <= t }.count
+  p oranges.select { |orange| s <= b+orange && b+orange <= t}.count
+end
+
+st = gets.rstrip.split
+
+s = st[0].to_i
+
+t = st[1].to_i
+
+ab = gets.rstrip.split
+
+a = ab[0].to_i
+
+b = ab[1].to_i
+
+mn = gets.rstrip.split
+
+m = mn[0].to_i
+
+n = mn[1].to_i
+
+apples = gets.rstrip.split(' ').map(&:to_i)
+
+oranges = gets.rstrip.split(' ').map(&:to_i)
+
+countApplesAndOranges s, t, a, b, apples, oranges
